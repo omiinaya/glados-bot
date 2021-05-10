21ae323fb0964ab8ac085439366920c0b35e3296
Omar Minaya <omiinaya@gmail.com>
2021-05-10 11:58:41 -0400

    simplified bot further

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => console.log('The bot has been started!'));
 
-client.on('messageReactionAdd', async (reaction, user) => {
+client.on('messageReactionAdd', (reaction, user) => {
+  toggleRole(reaction, user, 'add')
+});
+
+client.on('messageReactionRemove', (reaction, user) => {
+  toggleRole(reaction, user, 'remove')
+});
+
+async function toggleRole(reaction, user, action) {
   if (reaction.message.partial) await reaction.message.fetch();
   if (reaction.partial) await reaction.fetch();
   if (user.bot) return;
@@ -20,27 +28,8 @@ client.on('messageReactionAdd', async (reaction, user) => {
     for (const key in emojis) {
       if (reaction.emoji.id === `${emojis[key]}`) {
         var currentEmoji = `${key}`
-        await reaction.message.guild.members.cache
-        .get(user.id)
-        .roles.add(roles[currentEmoji]);
+        await reaction.message.guild.members.cache.get(user.id).roles[action](roles[currentEmoji]);
       }
     }
   } else return;
-});
-
-client.on('messageReactionRemove', async (reaction, user) => {
-  if (reaction.message.partial) await reaction.message.fetch();
-  if (reaction.partial) await reaction.fetch();
-  if (user.bot) return;
-  if (!reaction.message.guild) return;
-  if (reaction.message.channel.id == config.channel) {
-    for (const key in emojis) {
-      if (reaction.emoji.id === `${emojis[key]}`) {
-        var currentEmoji = `${key}`
-        await reaction.message.guild.members.cache
-        .get(user.id)
-        .roles.remove(roles[currentEmoji]);
-      }
-    }
-  } else return;
-});
+}
