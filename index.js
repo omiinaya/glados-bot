d246e67546d2103ff1597426e7ac603c393ce066
Omar Minaya <omiinaya@gmail.com>
2021-05-11 10:41:17 -0400

    added clearchat command.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require("dotenv").config()
 
 const Discord = require("discord.js");
-const fs      = require("fs");
-const path    = require("path");
-const i18n    = require("i18n");
+const fs = require("fs");
+const path = require("path");
+const i18n = require("i18n");
 
 const client = new Discord.Client({
   disableMentions: "everyone",
@@ -14,11 +14,28 @@ const client = new Discord.Client({
 const { config, emojis, roles, music } = require('./config')
 const PREFIX = music.prefix
 
-const startRolesModule  = require('./modules/roles')
-const startMusicModule  = require('./modules/music')
+const startRolesModule = require('./modules/roles')
+const startMusicModule = require('./modules/music')
 const startTwitchModule = require('./modules/twitch')
 
 client.login(process.env.BOT_TOKEN);
+
+client.on("message", msg => {
+  if (msg.content.toLowerCase().startsWith(PREFIX + "clearchat")) {
+    if (msg.member.roles.cache.some(role => role.name === 'Sullen')) {
+      msg.channel.send('test')
+
+      async function clear() {
+        msg.delete();
+        const fetched = await msg.channel.messages.fetch({ limit: 99 });
+        msg.channel.bulkDelete(fetched);
+      }
+      clear();
+    } else {
+      msg.channel.send('test')
+    }
+  }
+});
 
 client.on('ready', () => {
   startRolesModule(client, config, emojis, roles)
